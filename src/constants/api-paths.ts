98259f8a9c4b1f0edbb58612e0516907460738b5
arsenitem <arsenitem@bk.ru>
2022-07-30 00:05:23 +0300

    update upload api path

--- src/constants/api-paths.ts
+++ src/constants/api-paths.ts
@@ -1,7 +1,7 @@
 export const API_PATHS = {
 	product: 'https://tmy34d1i92.execute-api.eu-west-1.amazonaws.com/dev',
 	order: 'https://tmy34d1i92.execute-api.eu-west-1.amazonaws.com/dev',
-	import: 'https://tmy34d1i92.execute-api.eu-west-1.amazonaws.com/dev',
+	import: 'https://udat6rj5j4.execute-api.eu-west-1.amazonaws.com/dev',
 	bff: 'https://tmy34d1i92.execute-api.eu-west-1.amazonaws.com/dev',
 	cart: 'https://tmy34d1i92.execute-api.eu-west-1.amazonaws.com/dev',
 };
